5fd5e8c8ac6460a1ec3d4e1b9ef21b0937d883a8
Sven Ruppert <sven.ruppert@gmail.com>
2017-09-28 15:06:40 +0200

    checkstyle and version downgrade depending jUnit5
    http://junit.org/junit5/docs/current/user-guide/#running-tests-build-maven

--- modules/app/backend/rest/endpoint/src/main/java/org/openherbarium/module/backend/rest/endpoint/security/login/LoginServiceREST.java
+++ modules/app/backend/rest/endpoint/src/main/java/org/openherbarium/module/backend/rest/endpoint/security/login/LoginServiceREST.java
@@ -3,7 +3,6 @@ package org.openherbarium.module.backend.rest.endpoint.security.login;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -23,10 +22,10 @@ public class LoginServiceREST {
   @GET()
   @Path("{checkLogin}")
   @Produces(MediaType.APPLICATION_JSON)
-  public String checkLogin(@QueryParam("username") final String username,
-                           @QueryParam("password") final String password ){
+  public String checkLogin(@QueryParam("username") final String username ,
+                           @QueryParam("password") final String password) {
 
-    return new Gson().toJson(loginService.checkLogin(username, password),Boolean.class);
+    return new Gson().toJson(loginService.checkLogin(username , password) , Boolean.class);
   }
 
 }
