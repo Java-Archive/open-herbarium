4f2a6da0cdee57448be0a8438424a19d2ed4c57c
marcus.lang <m.lang@reply.de>
2017-10-01 08:28:40 +0200

    smaller changes at imageservice

---
+++ modules/app/srv/imageservice/rest/endpoint/src/main/java/org/openherbarium/module/srv/imageservice/rest/endpoint/srv/imageservice/rest/client/ImageServiceRestEndpoint.java
@@ -0,0 +1,10 @@
+package org.openherbarium.module.srv.imageservice.rest.endpoint.srv.imageservice.rest.client;
+
+public class ImageServiceRestEndpoint {
+
+  // TODO
+  // getImage()
+  // updateImage(String id)
+  // removeImage(String id)
+
+}
