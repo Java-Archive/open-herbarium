b52bfe99c37e7f0d8243b9c8d3c3e34433f775e8
dve <dve@vergien.net>
2017-10-13 10:31:45 +0200

    add date to metadata model

--- modules/app/srv/metadataservice/rest/endpoint/src/test/resources/demoData.sql
+++ modules/app/srv/metadataservice/rest/endpoint/src/test/resources/demoData.sql
@@ -3,8 +3,8 @@ INSERT INTO people (external_id, last_name, first_name) VALUES (4712, 'Jansen', 
 
 -- Taxon Id 1137: Carex bohemica
 -- Taxon Id 21023: Carex flavella 
-INSERT INTO metadata (external_id, taxon_id, determiner, recorder) VALUES ('47659', '1137', 1, 2);
-INSERT INTO metadata (external_id, taxon_id, determiner, recorder) VALUES ('47271', '21023', 2, 1);
+INSERT INTO metadata (external_id, taxon_id, date, determiner, recorder) VALUES ('47659', '1137', '1970-03-04', 1, 2);
+INSERT INTO metadata (external_id, taxon_id, date, determiner, recorder) VALUES ('47271', '21023','1800-10-09', 2, 1);
 
 INSERT INTO scans (name) VALUES ('Care_bohe_GFW_47659');
 INSERT INTO scans (name) VALUES ('Care_flav_GFW_47271');
