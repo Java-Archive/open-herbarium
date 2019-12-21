305afd0102263454d92679c432c3a4b600f890b9
Sven Ruppert <sven.ruppert@gmail.com>
2019-12-21 23:26:44 +0100

    activated production mode

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # open-herbarium
 
-[![pipeline status](https://git.loe.auf.uni-rostock.de/werbeo/open-herbarium/badges/develop/pipeline.svg)](https://git.loe.auf.uni-rostock.de/werbeo/open-herbarium/commits/develop) [![Quality Gate](https://sonar.loe.auf.uni-rostock.de/api/badges/gate?key=org.open-herbarium:herbarium-parent)](https://sonar.loe.auf.uni-rostock.de/dashboard/index/org.open-herbarium:herbarium-parent)
+## Used Components
+
+* NanoVaadin(Undertow/Jetty)
+* i18nProvider
+* i18nPageTitle
+* IDGenerator
+* FlowSecurity
--- src/main/java/org/openherbarium/webapp/UIService.java
+++ src/main/java/org/openherbarium/webapp/UIService.java
@@ -1,5 +1,6 @@
 package org.openherbarium.webapp;
 
+import com.vaadin.flow.server.Constants;
 import org.apache.commons.cli.ParseException;
 import org.rapidpm.frp.model.Result;
 import org.rapidpm.vaadin.nano.CoreUIServiceJava;
@@ -12,8 +13,9 @@ public class UIService
   static Result<UIService> uiService = Result.failure("not initialized");
 
   public static void main(String[] args) throws ParseException {
-    System.setProperty(SERVLET_PARAMETER_ENABLE_DEV_SERVER, "false");
-    System.setProperty("vaadin.compatibilityMode", "true");
+    System.setProperty(Constants.SERVLET_PARAMETER_ENABLE_DEV_SERVER, "false");
+    System.setProperty("vaadin.compatibilityMode", "false");
+    System.setProperty("vaadin.productionMode", "true");
     System.setProperty(CORE_UI_BASE_PKG, "org.openherbarium");
 
 
