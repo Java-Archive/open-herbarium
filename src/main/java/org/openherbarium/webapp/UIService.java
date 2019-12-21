4f2264fe6177fc8967aee5218dde83eacd942f94
Sven Ruppert <sven.ruppert@gmail.com>
2019-12-21 15:47:27 +0100

    start next generation

--- src/main/java/org/openherbarium/webapp/UIService.java
+++ src/main/java/org/openherbarium/webapp/UIService.java
@@ -4,12 +4,15 @@ import org.apache.commons.cli.ParseException;
 import org.rapidpm.frp.model.Result;
 import org.rapidpm.vaadin.nano.CoreUIServiceJava;
 
+import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_ENABLE_DEV_SERVER;
+
 public class UIService
     extends CoreUIServiceJava {
 
   static Result<UIService> uiService = Result.failure("not initialized");
 
   public static void main(String[] args) throws ParseException {
+    System.setProperty(SERVLET_PARAMETER_ENABLE_DEV_SERVER, "false");
     System.setProperty(CORE_UI_BASE_PKG, "org.openherbarium");
 
 
